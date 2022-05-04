60284f2e6f4f99d77ece8126cf633eaf6f5d832f
Juhwan <dev.juhwanlee@gmail.com>
2022-05-04 21:05:13 +0900

    이주환 edit : github action을 위한 yml 파일 수정

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,15 @@ import 'tailwindcss/tailwind.css';
 import Community from './pages/Community';
 
 function App() {
+	const onClickHandler = () => {
+		window.alert('test');
+	};
 	return (
 		<div>
 			<Community />
 			app
+			<br />
+			<button onClick={onClickHandler}>testBtn</button>
 		</div>
 	);
 }
