9aa16852b28cc73d2f10f6ad2abcc8ea6832dadf
Juhwan <dev.juhwanlee@gmail.com>
2022-05-05 18:42:59 +0900

    이주환 feat: router 기본 설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,16 @@
 import 'tailwindcss/tailwind.css';
+import { Routes, Route } from 'react-router-dom';
+import Main from './pages/Main';
 import Community from './pages/Community';
+import Login from './pages/Login';
 
 function App() {
-  const onClickHandler = () => {
-    window.alert('test');
-  };
   return (
-    <div>
-      <Community />
-      app
-      <br />
-      <button onClick={onClickHandler}>testBTN</button>
-    </div>
+    <Routes>
+      <Route path='/' element={<Main />} />
+      <Route path='/login' element={<Login />} />
+      <Route path='/community' element={<Community />} />
+    </Routes>
   );
 }
 
--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './redux/configureStore';
+
 // import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>
+	<BrowserRouter>
+		<Provider store={store}>
+			<App />
+		</Provider>
+	</BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
