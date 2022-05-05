29f6c50f34b6f7ce5cc0213b37c7db558b435841
Juhwan <dev.juhwanlee@gmail.com>
2022-05-05 20:03:41 +0900

    이주환 style : global style을 위한 css 파일 추가

--- src/index.jsx
+++ src/index.jsx
@@ -6,14 +6,15 @@ import { Provider } from 'react-redux';
 import store from './redux/configureStore';
 
 // import reportWebVitals from './reportWebVitals';
+import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-	<BrowserRouter>
-		<Provider store={store}>
-			<App />
-		</Provider>
-	</BrowserRouter>
+  <BrowserRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
