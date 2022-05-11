4e76c8abc451dd599b717b39e1a76544f11cada7
Juhwan <dev.juhwanlee@gmail.com>
2022-05-12 06:47:31 +0900

    이주환 style : img로 불러온 svg 파일 색상 수정 테스트 #14 close

--- src/components/BottomNav.tsx
+++ src/components/BottomNav.tsx
@@ -10,7 +10,14 @@ const BottomNav = () => {
   return (
     <>
       <button className='fixed w-12 h-12 rounded-full z-10 bg-slate-400 bottom-104 right-6'>
-        <img className='mx-auto' src={share} />
+        <img
+          className='mx-auto '
+          src={share}
+          style={{
+            filter:
+              'invert(95%) sepia(0%) saturate(21%) hue-rotate(357deg) brightness(104%) contrast(108%)',
+          }}
+        />
       </button>
       <nav className='fixed flex bottom-0 bg-slate-50 w-full h-84'>
         <button className='flex-auto fill-slate-200'>
