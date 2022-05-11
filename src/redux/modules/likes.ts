ffcfd337577a6c43e23e878f7004d8065068d2ec
cpk <cpk0709@gmail.com>
2022-05-11 15:07:53 +0900

    최필경 feat:좋아요 기능 구현중

--- src/redux/modules/likes.ts
+++ src/redux/modules/likes.ts
@@ -1,3 +1,18 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import axios from 'axios';
 
-const INITIAL_STATE = {};
+export interface LikesState {
+  like: {
+    postsId: number;
+    nickname: string;
+    likesId: number;
+  };
+}
+
+const INITIAL_STATE: LikesState = {
+  like: {
+    postsId: 1,
+    nickname: '',
+    likesId: 1,
+  },
+};
