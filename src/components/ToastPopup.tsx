34fdfba99ed849e8873c839d62bcfaae9df7ad76
Juhwan <dev.juhwanlee@gmail.com>
2022-05-14 07:41:56 +0900

    이주환 feat : 토스트 팝업 기능 구현

--- src/components/ToastPopup.tsx
+++ src/components/ToastPopup.tsx
@@ -1,19 +1,46 @@
+import { useState } from 'react';
 import { Button, DarkWrapper } from './atoms';
+import { useNavigate } from 'react-router-dom';
 
 const ToastPopup = () => {
+  const navigate = useNavigate();
+  const [isActivePopup, setIsActivePopup] = useState(false);
+
+  const handleClosePopup = () => {
+    setIsActivePopup(false);
+  };
+  const handleIsActivePopup = () => {
+    setIsActivePopup(true);
+  };
+  const handleToLogin = () => {
+    navigate('../');
+  };
+
   return (
     <>
-      <DarkWrapper />
-      <div className='absolute z-20 w-full bottom-0 bg-white pb-84px px-30px rounded-t-40px pt-34px'>
-        <div>
-          <p>잠깐, 로그인 완료 후</p>
-          <p>Copick을 이용해 주세요!</p>
-          <p>
-            간편 로그인을 통해 Copick 안에 있는 모든 서비스를 즐기실 수 있어요
-          </p>
-        </div>
-        <Button>로그인하기</Button>
+      {/* 로그인하면 로그인 유무에 따라 로직 수정 예정 */}
+      <div
+        className='absolute top-0 left-1/3 bg-amber-500'
+        onClick={handleIsActivePopup}
+      >
+        toastPopup testBtn
       </div>
+      {isActivePopup ? (
+        <>
+          <DarkWrapper onClick={handleClosePopup} />
+          <div className='absolute z-20 w-full bottom-0 bg-white pb-84px px-30px rounded-t-40px pt-34px'>
+            <div>
+              <p>잠깐, 로그인 완료 후</p>
+              <p>Copick을 이용해 주세요!</p>
+              <p>
+                간편 로그인을 통해 Copick 안에 있는 모든 서비스를 즐기실 수
+                있어요
+              </p>
+            </div>
+            <Button onClick={handleToLogin}>로그인하기</Button>
+          </div>
+        </>
+      ) : null}
     </>
   );
 };
--- src/components/atoms/DarkWrapper.tsx
+++ src/components/atoms/DarkWrapper.tsx
@@ -1,7 +1,14 @@
-const DarkWrapper = () => {
+interface Wrapper {
+  onClick: () => void;
+}
+
+const DarkWrapper = (props: Wrapper) => {
   return (
     // eslint-disable-next-line
-    <div className='absolute top-0 left-0 w-full h-full bg-[rgba(11,11,11,0.45)]'></div>
+    <div
+      className='absolute top-0 left-0 w-full h-full bg-[rgba(11,11,11,0.45)]'
+      onClick={props.onClick}
+    ></div>
   );
 };
 
