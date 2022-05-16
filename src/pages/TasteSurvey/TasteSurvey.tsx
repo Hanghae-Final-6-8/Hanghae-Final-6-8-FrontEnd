5335968c1c95fdeff5e58cd3273efe9c28b87d36
Juhwan <dev.juhwanlee@gmail.com>
2022-05-17 05:28:01 +0900

    이주환 feat : 취향조사 페이지 기본 라우팅 처리

--- src/pages/TasteSurvey/TasteSurvey.tsx
+++ src/pages/TasteSurvey/TasteSurvey.tsx
@@ -1,16 +1,24 @@
 import { Outlet } from 'react-router-dom';
-import { cross } from '../../assets/icons';
+import { cross, left } from '../../assets/icons';
 import { useNavigate } from 'react-router-dom';
 
 const TasteSurvey = () => {
   const navigate = useNavigate();
   const handleToMain = () => {
-    navigate('../main');
+    navigate('../main', { replace: true });
+  };
+
+  const handleToPrevPage = () => {
+    // 일단 뒤로가기 처리를 해놨으나 로직에 따라 변경할 수 있습니다.
+    navigate(-1);
   };
 
   return (
     <>
       <div className='relative h-full'>
+        <button className='absolute top-3.5 left-0'>
+          <img className='w-8' src={left} onClick={handleToPrevPage} />
+        </button>
         <button className='absolute top-3.5 right-0' onClick={handleToMain}>
           <img className='w-8' src={cross} />
         </button>
