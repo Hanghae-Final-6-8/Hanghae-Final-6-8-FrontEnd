5a90c93907f6b6b1f51c4fb8277d3334376f9567
Juhwan <dev.juhwanlee@gmail.com>
2022-05-17 04:02:49 +0900

    이주환 style : logo svg 추가

---
+++ src/assets/logo/index.ts
@@ -0,0 +1,5 @@
+import logoCopick from './logoCopick.svg';
+import logoCopickSqure from './logoCopickSqure.svg';
+import pickYourCoffe from './pickYourCoffe.svg';
+
+export { logoCopick, logoCopickSqure, pickYourCoffe };
