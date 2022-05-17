910aea4bb453758a7831d4a9edadb9d165e735e8
Juhwan <dev.juhwanlee@gmail.com>
2022-05-17 20:49:48 +0900

    이주환 feat : access, refresh 토큰 인증

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,41 +1,56 @@
 import axios, { AxiosRequestConfig, AxiosResponse } from 'axios';
-import { getAccessTokenFromCookie } from '../utils/cookie';
-
-// 임시로 구현하였습니다.
+import {
+  getAccessTokenFromCookie,
+  getRefreshTokenFromCookie,
+} from '../utils/cookie';
+import { setMoveToLogin } from '../utils/setMoveToLogin';
 
 const baseURL = process.env.REACT_APP_BASE_URL;
+const accessToken = getAccessTokenFromCookie();
 
 const instance = axios.create({
   baseURL: baseURL,
-  // withCredentials: true,
   headers: {
     withCredentials: true,
+    ACCESS_TOKEN: `${accessToken}`,
   },
+  timeout: 3000,
 });
 
-const setHeaderAccessToken = (token: string) => {
-  instance.defaults.headers.common['ACCESS_TOKEN'] = `Bearer ${token}`;
-};
-
-const setHeaderRefreshToken = (token: string) => {
-  instance.defaults.headers.common['REFRESH_TOKEN'] = `Bearer ${token}`;
-};
-
-// const checkToken = (config: AxiosRequestConfig) => {};
-
 instance.interceptors.request.use((config: AxiosRequestConfig) => {
-  const accessToken = getAccessTokenFromCookie();
-  accessToken && setHeaderAccessToken(accessToken);
+  console.log('request config입니다 \n', config);
   return config;
 });
 
 instance.interceptors.response.use(
   (response: AxiosResponse) => {
-    console.log(response);
+    console.log('response입니다 \n', response);
     return response;
   },
   (error) => {
-    console.log(error);
+    const {
+      data: responseData,
+      config: originalRequest,
+      status: statusCode,
+    } = error.response;
+
+    if (statusCode === 441) {
+      const refreshToken = getRefreshTokenFromCookie();
+      originalRequest.headers['REFRESH_TOKEN'] = refreshToken;
+      return axios(originalRequest);
+    }
+    if (statusCode === 442) {
+      const accessToken = getAccessTokenFromCookie();
+      originalRequest.heders['ACCESS_TOKEN'] = accessToken;
+      return axios(originalRequest);
+    }
+    if (statusCode === 443) {
+      setMoveToLogin();
+      return Promise.reject(error);
+    }
+
+    console.log(responseData, originalRequest, statusCode);
+
     return error;
   }
 );
--- src/utils/cookie.ts
+++ src/utils/cookie.ts
@@ -3,12 +3,16 @@ import { Cookies } from 'react-cookie';
 const cookies = new Cookies();
 
 // 백에서 자동으로 jwt 토큰을 넣어주지 않을 경우를 위해 setCookie를 만들어두었습니다.
-export const setAccessTokenFromCookie = (value: string) => {
-  return cookies.set('ACCESS_TOKEN', value);
+export const setAccessTokenToCookie = (value: string) => {
+  return cookies.set('ACCESS_TOKEN', `Bearer ${value}`, {
+    path: '/',
+  });
 };
 
-export const setRefreshTokenFromCookie = (value: string) => {
-  return cookies.set('REFRESH_TOKEN', value);
+export const setRefreshTokenToCookie = (value: string) => {
+  return cookies.set('REFRESH_TOKEN', `Bearer ${value}`, {
+    path: '/',
+  });
 };
 
 // 반환 값이 필요하기 때문에 return이 필요합니다.
