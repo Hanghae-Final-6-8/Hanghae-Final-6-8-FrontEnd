e60c7c8fbd255d17e9f2413597d26f844d0d51f7
Juhwan <dev.juhwanlee@gmail.com>
2022-05-19 05:07:45 +0900

    이주환 feat : 취향조사 결과 페이지 초기 테스트

--- src/pages/TasteSurvey/TasteSurveyLoading.tsx
+++ src/pages/TasteSurvey/TasteSurveyLoading.tsx
@@ -1,4 +1,10 @@
+import { useLocation } from 'react-router-dom';
+
 const TasteSurveyLoading = () => {
+  const location = useLocation();
+  const tasteList = location.state;
+  console.log(tasteList);
+
   return (
     <>
       <div>Loading 페이지</div>
