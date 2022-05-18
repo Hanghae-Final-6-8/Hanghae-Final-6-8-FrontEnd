7eb594f27ef14071e9fe289c6edab353cb376e5a
Juhwan <dev.juhwanlee@gmail.com>
2022-05-19 04:44:36 +0900

    이주환 fix : 로컬 작업을 위한 임시 cookie 설정

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,3 +1,7 @@
+import {
+  setAccessTokenToCookie,
+  setRefreshTokenToCookie,
+} from './../utils/cookie';
 import axios, { AxiosRequestConfig, AxiosResponse } from 'axios';
 import {
   getAccessTokenFromCookie,
@@ -16,17 +20,27 @@ const instance = axios.create({
 });
 
 instance.interceptors.request.use((config: AxiosRequestConfig) => {
-  const accessToken = getAccessTokenFromCookie();
-  const refreshToken = getRefreshTokenFromCookie();
+  // const accessToken = getAccessTokenFromCookie();
   config.headers!['Content-Type'] = 'application/json; charset=utf-8';
   config.headers!['Access-Control-Allow-Origin'] = '*';
   config.headers!['Access-Control-Allow-Credentials'] = true;
   config.headers!['Authorization'] = `Bearer ${accessToken}`;
-  config.headers!['REFRESH_TOKEN'] = `Bearer ${refreshToken}`;
   (config.headers!.withCredentials = true),
     console.log('request config입니다 \n', config);
   return config;
 });
+
+/****************************************************************************
+ * localhost에서 작업을 위한 임시 처리 - 홈페이지에서 쿠키 긁어와서 넣으면 됩니다.
+ * 1. 홈페이지 접속 후 로그인
+ * 2. Application에서 쿠키를 복사해서 아래에 입력해줍니다.
+ * 3. 아래 주석 처리 된 부분을 '주석 해제' -> '저장' 1회
+ * 4. 그후 다시 '주석 처리' -> '쿠키 삭제' -> '저장' 1회
+ * 5. 이런 식으로 하면 됩니다.
+ * 6. 임시적으로 로컬에서 작업을 하기 위해 이런식으로 하고 있습니다.
+ ***************************************************************************/
+setAccessTokenToCookie('');
+setRefreshTokenToCookie('');
 
 instance.interceptors.response.use(
   (response: AxiosResponse) => {
@@ -39,27 +53,27 @@ instance.interceptors.response.use(
       config: originalRequest,
       status: statusCode,
     } = error.response;
-    // if (statusCode === 440) {
-    //   removeCookies();
-    //   setMoveToLogin();
-    //   return Promise.reject(error);
-    // }
+    if (statusCode === 440) {
+      removeCookies();
+      setMoveToLogin();
+      return Promise.reject(error);
+    }
 
-    // if (statusCode === 441) {
-    //   const refreshToken = getRefreshTokenFromCookie();
-    //   originalRequest.headers['REFRESH_TOKEN'] = `Bearer ${refreshToken}`;
-    //   return axios(originalRequest);
-    // }
-    // if (statusCode === 442) {
-    //   const accessToken = getAccessTokenFromCookie();
-    //   originalRequest.headers['ACCESS_TOKEN'] = `Bearer ${accessToken}`;
-    //   return axios(originalRequest);
-    // }
-    // if (statusCode === 443) {
-    //   removeCookies();
-    //   setMoveToLogin();
-    //   return Promise.reject(error);
-    // }
+    if (statusCode === 441) {
+      // const refreshToken = getRefreshTokenFromCookie();
+      originalRequest.headers['Authorization'] = `Bearer ${refreshToken}`;
+      return axios(originalRequest);
+    }
+    if (statusCode === 442) {
+      // const accessToken = getAccessTokenFromCookie();
+      originalRequest.headers['Authorization'] = `Bearer ${accessToken}`;
+      return axios(originalRequest);
+    }
+    if (statusCode === 443) {
+      removeCookies();
+      setMoveToLogin();
+      return Promise.reject(error);
+    }
 
     console.log(responseData, originalRequest, statusCode);
 
