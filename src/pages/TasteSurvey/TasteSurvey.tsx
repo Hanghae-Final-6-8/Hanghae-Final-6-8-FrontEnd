5fce832aca58aa112031d0601257332cfca76f0c
Juhwan <dev.juhwanlee@gmail.com>
2022-05-19 07:30:51 +0900

    이주환 feat : 취향조사 페이지에 따른 버튼 비쥬얼라이징 처리

--- src/pages/TasteSurvey/TasteSurvey.tsx
+++ src/pages/TasteSurvey/TasteSurvey.tsx
@@ -1,9 +1,11 @@
 import { Outlet } from 'react-router-dom';
 import { cross, left } from '../../assets/icons';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 
 const TasteSurvey = () => {
   const navigate = useNavigate();
+  const location = useLocation();
   const handleToMain = () => {
     navigate('../main', { replace: true });
   };
@@ -13,15 +15,34 @@ const TasteSurvey = () => {
     navigate(-1);
   };
 
+  // 현재 pathname을 가져오기 위한 useEffect 사용
+  const [isUselessPath, setIseUselessPath] = useState(false);
+  useEffect(() => {
+    const currentLocation = location.pathname;
+    console.log(currentLocation);
+    if (
+      currentLocation === '/survey/loading' ||
+      currentLocation === '/survey/needlogin'
+    ) {
+      setIseUselessPath(true);
+    } else {
+      setIseUselessPath(false);
+    }
+  }, [location.pathname]);
+
   return (
     <>
       <div className='relative h-full'>
-        <button className='absolute top-3.5 left-0'>
-          <img className='w-8' src={left} onClick={handleToPrevPage} />
-        </button>
-        <button className='absolute top-3.5 right-0' onClick={handleToMain}>
-          <img className='w-8' src={cross} />
-        </button>
+        {!isUselessPath ? (
+          <>
+            <button className='absolute top-3.5 left-0'>
+              <img className='w-8' src={left} onClick={handleToPrevPage} />
+            </button>
+            <button className='absolute top-3.5 right-0' onClick={handleToMain}>
+              <img className='w-8' src={cross} />
+            </button>
+          </>
+        ) : null}
         <Outlet />
       </div>
     </>
