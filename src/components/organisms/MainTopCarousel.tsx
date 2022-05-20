33cd76f9ca7f0cf4487e72858dfd2cfa6c4bf351
Juhwan <dev.juhwanlee@gmail.com>
2022-05-20 12:24:10 +0900

    이주환 feat : 메인 임시 캐러셀 구현

--- src/components/organisms/MainTopCarousel.tsx
+++ src/components/organisms/MainTopCarousel.tsx
@@ -1,5 +1,29 @@
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { cardImg01, cardImg02, cardImg03 } from '../../assets/images';
+
+// Import Swiper styles
+import 'swiper/css';
+
 const MainTopCarousel = () => {
-  return <>캐러셀</>;
+  return (
+    <>
+      <Swiper
+        effect={'cards'}
+        grabCursor={true}
+        className='mySwiper w-[220px] h-[320px]'
+      >
+        <SwiperSlide className='bg-cardImg01 bg-no-repeat rounded-[32px] '>
+          {/*  */}
+        </SwiperSlide>
+        <SwiperSlide className='bg-cardImg02 bg-no-repeat rounded-[32px] '>
+          {/*  */}
+        </SwiperSlide>
+        <SwiperSlide className='bg-cardImg03 bg-no-repeat rounded-[32px] '>
+          {/*  */}
+        </SwiperSlide>
+      </Swiper>
+    </>
+  );
 };
 
 export default MainTopCarousel;
