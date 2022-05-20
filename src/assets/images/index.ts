c8a7e7fab01bd1f17b6317837e78702a5b6d7afa
Juhwan <dev.juhwanlee@gmail.com>
2022-05-20 09:42:06 +0900

    이주환 chore : 예시 img 추가

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -1,5 +1,17 @@
 import imgExample from './img-example.png';
 import coffee_default from './coffee_default.png';
 import tasteSurveyMain from './tasteSurveyMain.png';
+import cardImg01 from './cardImg01.png';
+import cardImg02 from './cardImg02.png';
+import cardImg03 from './cardImg03.png';
+import mainHeaderImg from './mainHeaderImg.png';
 
-export { imgExample, coffee_default, tasteSurveyMain };
+export {
+  imgExample,
+  coffee_default,
+  tasteSurveyMain,
+  cardImg01,
+  cardImg02,
+  cardImg03,
+  mainHeaderImg,
+};
