84f9758ba16ce5916ea80c9d5234a34eb47ea4c4
Juhwan <dev.juhwanlee@gmail.com>
2022-05-20 12:23:26 +0900

    이주환 chore : 이미지 수정

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -1,8 +1,8 @@
 import imgExample from './img-example.png';
 import coffee_default from './coffee_default.png';
 import tasteSurveyMain from './tasteSurveyMain.png';
-import cardImg01 from './cardImg01.png';
-import cardImg02 from './cardImg02.png';
+import cardImg01 from './cardImg001.png';
+import cardImg02 from './cardImg002.png';
 import cardImg03 from './cardImg03.png';
 import mainHeaderImg from './mainHeaderImg.png';
 
