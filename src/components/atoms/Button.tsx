22c5a485c8340781d977a062dae37fad034882e6
Juhwan <dev.juhwanlee@gmail.com>
2022-05-23 06:23:21 +0900

    이주환 feat : beans list 카페 버튼 뷰 완성

--- src/components/atoms/Button.tsx
+++ src/components/atoms/Button.tsx
@@ -15,20 +15,29 @@ const Button = (props: BtnProps) => {
   const tasteSurveyNoneActive = props.type === 'tasteSurveyNoneActive';
   const toastPopup = props.type === 'toastPopup';
 
+  const selectBtn = props.type === 'selectBtn';
+
   return (
     <button
-      className={classnames(`w-full rounded-btn py-2.5 ${className}`, {
-        'mt-4': className === undefined && props.type === undefined,
-        'bg-brownP bg-cover': className && props.type === undefined,
-        'bg-gray60 text-gray20': props.className === undefined,
-        'bg-brownP bg-cover mt-30px': bgBrownP,
-        'absolute font-500 text-sub2 bottom-2 bg-brownP bg-cover shadow-tasteBrown':
-          tasteSurvey,
-        'absolute font-500 text-sub2 bottom-2 bg-gray30 shadow-tasteBrown':
-          tasteSurveyNoneActive,
-        'bg-brownP bg-cover mt-30px mb-34px text-white shadow-tasteBrown text-body font-500':
-          toastPopup,
-      })}
+      className={classnames(
+        `w-full rounded-btn py-2.5 ${className}`,
+        {
+          'mt-4': className === undefined && props.type === undefined,
+          'bg-brownP bg-cover': className && props.type === undefined,
+          'bg-gray60 text-gray20': className && props.type === undefined,
+          'bg-brownP bg-cover mt-30px': bgBrownP,
+          'absolute font-500 text-sub2 bottom-2 bg-brownP bg-cover shadow-tasteBrown':
+            tasteSurvey,
+          'absolute font-500 text-sub2 bottom-2 bg-gray30 shadow-tasteBrown':
+            tasteSurveyNoneActive,
+          'bg-brownP bg-cover mt-30px mb-34px text-white shadow-tasteBrown text-body font-500':
+            toastPopup,
+        },
+        {
+          'shadow-tasteBrown text-body mb-[3px] bg-white text-gray60':
+            selectBtn,
+        }
+      )}
       onClick={props.onClick}
     >
       <Text btnClassName={className} type={props.type}>
--- src/components/organisms/BeansToastPopup.tsx
+++ src/components/organisms/BeansToastPopup.tsx
@@ -1,13 +1,33 @@
-import { ToastPopupBox, DarkWrapper } from '../atoms';
+import { check } from '../../assets/icons';
+import { ToastPopupBox, DarkWrapper, Text, Span, Button } from '../atoms';
 
 interface BeansToastPopupProps {
   onClick: () => void;
 }
 
 const BeansToastPopup = (props: BeansToastPopupProps) => {
+  const cafeListFormdata = [
+    { cafe_id: 1, cafe_name: '스타벅스' },
+    { cafe_id: 2, cafe_name: '투썸플레이스' },
+    { cafe_id: 3, cafe_name: '탐앤탐스' },
+    { cafe_id: 4, cafe_name: '엔제리너스' },
+  ];
+
   return (
     <DarkWrapper onClick={props.onClick}>
-      <ToastPopupBox>{/*  */}</ToastPopupBox>
+      <ToastPopupBox type='default'>
+        <div className='flex justify-between mb-30px'>
+          <Text className='font-500 text-sub'>
+            <Span type='strong'>찾으시는 카페</Span>를 선택해주세요!
+          </Text>
+          <img src={check} />
+        </div>
+        {cafeListFormdata.map((item) => (
+          <Button type='selectBtn' key={item.cafe_id}>
+            {item.cafe_name}
+          </Button>
+        ))}
+      </ToastPopupBox>
     </DarkWrapper>
   );
 };
