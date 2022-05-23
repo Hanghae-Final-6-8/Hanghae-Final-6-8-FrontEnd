292d1356493e59404144782d2325db3ed4414efb
Juhwan <dev.juhwanlee@gmail.com>
2022-05-23 12:26:53 +0900

    이주환 feat : bottom nav 동적할당

--- src/components/atoms/Icon.tsx
+++ src/components/atoms/Icon.tsx
@@ -2,12 +2,25 @@ import classnames from 'classnames';
 
 interface IconProps {
   src: string;
+  type?: string;
+  className?: string;
+  isClicked?: boolean;
 }
 
 const Icon = (props: IconProps) => {
+  const className = props.className;
+  const bottomNav = props.type === 'bottomNav';
+  const isClicked = props.isClicked === true;
+
   return (
     <>
-      <img src={props.src} />
+      <img
+        className={classnames(`${className}`, {
+          'mx-auto w-30px -translate-y-7px': bottomNav,
+          'filter-gray30': !isClicked,
+        })}
+        src={props.src}
+      />
     </>
   );
 };
