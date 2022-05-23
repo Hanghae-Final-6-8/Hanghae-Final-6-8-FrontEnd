d77ff9807046683fe2fb285a55e44e93cab1fcf6
Juhwan <dev.juhwanlee@gmail.com>
2022-05-23 13:32:22 +0900

    이주환 style : taste survey botton 스타일 추가

--- src/components/atoms/Button.tsx
+++ src/components/atoms/Button.tsx
@@ -27,11 +27,11 @@ const Button = (props: BtnProps) => {
           'bg-brownP bg-cover': className && props.type === undefined,
           'bg-gray60 text-gray20': className && props.type === undefined,
           'bg-brownP bg-cover mt-30px': bgBrownP,
-          'absolute font-500 text-sub2 bottom-2 bg-brownP bg-cover shadow-tasteBrown':
+          'absolute font-500 text-sub2 bottom-2 ext-white bg-brownP bg-cover shadow-tasteBrown text-white':
             tasteSurvey,
-          'absolute font-500 text-sub2 bottom-2 bg-gray30 shadow-tasteBrown':
+          'absolute font-500 text-sub2 bottom-2 text-white bg-gray30 shadow-tasteBrown':
             tasteSurveyNoneActive,
-          'bg-brownP bg-cover mt-30px mb-34px text-white shadow-tasteBrown text-body font-500':
+          'bg-brownP bg-cover mt-30px mb-34px  shadow-tasteBrown text-body font-500':
             toastPopup,
         },
         {
