224aab54d85d2e5f2329a4f63f5c71979933a156
cpk <cpk0709@gmail.com>
2022-05-25 12:37:12 +0900

    최필경 feat:내가 작성한 댓글리스트 뷰에 표시

--- src/components/organisms/MyActivity.tsx
+++ src/components/organisms/MyActivity.tsx
@@ -5,6 +5,7 @@ import { RootState } from '../../redux/configureStore';
 import { getPostListMine } from '../../redux/modules/mypage';
 import { getMyCommentDB } from '../../redux/modules/mypage';
 import { useNavigate } from 'react-router-dom';
+import { right } from '../../assets/icons';
 
 const MyActivity = () => {
   const navigate = useNavigate();
@@ -25,32 +26,59 @@ const MyActivity = () => {
   const handleMoveToDetailPage = (postsId: number) => {
     navigate(`/posts/${postsId}`);
   };
-  console.log(myCommentList);
+
   return (
-    <div className='flex flex-col pb-24'>
-      {listMyActivity.map((post, index) => {
-        return (
-          <div
-            className='m-2 flex'
-            key={index}
-            onClick={() => {
-              handleMoveToDetailPage(post.postsId!);
-            }}
-          >
-            <img
-              className='w-24'
-              src={
-                post.postsImage
-                  ? post.postsImage.toString()
-                  : 'https://cdn.pixabay.com/photo/2018/11/13/21/43/instagram-3814052__340.png'
-              }
-            />
-            <div>
-              <p>{post.content}</p>
+    <div>
+      <div className='flex flex-col'>
+        <div className='flex justify-between'>
+          <div className='mt-6 mb-6'>내가 작성한 글</div>
+          <button>
+            <img src={right} />
+          </button>
+        </div>
+
+        {listMyActivity.map((post, index) => {
+          return (
+            <div
+              className='m-2 flex'
+              key={index}
+              onClick={() => {
+                handleMoveToDetailPage(post.postsId!);
+              }}
+            >
+              <img
+                className='w-24'
+                src={
+                  post.postsImage
+                    ? post.postsImage.toString()
+                    : 'https://cdn.pixabay.com/photo/2018/11/13/21/43/instagram-3814052__340.png'
+                }
+              />
+              <div>
+                <p>{post.content}</p>
+              </div>
             </div>
-          </div>
-        );
-      })}
+          );
+        })}
+      </div>
+      <div className='flex flex-col pb-24'>
+        <div className='flex justify-between'>
+          <div className='mt-6 mb-6'>내가 작성한 댓글</div>
+          <button>
+            <img src={right} />
+          </button>
+        </div>
+        {myCommentList.map((comment, index) => {
+          return (
+            <div className='m-2 flex' key={index}>
+              <div className='flex justify-between w-full'>
+                <p>{comment.content}</p>
+                <button>더보기</button>
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
