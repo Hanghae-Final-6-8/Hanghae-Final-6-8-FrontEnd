6ec2e00695c97a3249d24d12f0425485af951993
Juhwan <dev.juhwanlee@gmail.com>
2022-05-25 14:48:41 +0900

    이주환 feat : 원두 상세 페이지 임시 예외 처리

--- src/pages/BeansList/BeanDetail.tsx
+++ src/pages/BeansList/BeanDetail.tsx
@@ -3,19 +3,20 @@ import { useAppDispatch } from '../../redux/configureStore';
 import { useLocation } from 'react-router-dom';
 import { useEffect } from 'react';
 import { detailBeans } from '../../redux/modules/beans';
+import { NotFound } from '../System';
 
 const BeanDetail = () => {
   const appDispatch = useAppDispatch();
   const { pathname } = useLocation();
   const beansNum = Number(pathname.split('/')[2]);
 
+  console.log(typeof beansNum);
+
   useEffect(() => {
     appDispatch(detailBeans(beansNum));
   });
   return (
-    <>
-      <MainYesTasteSurvey />
-    </>
+    <>{isNaN(beansNum) || beansNum ? <NotFound /> : <MainYesTasteSurvey />}</>
   );
 };
 
