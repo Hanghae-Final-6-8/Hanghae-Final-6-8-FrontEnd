9bb6a1376a624d46247f6bd35d0ddb4ab9f7fcb7
cpk <cpk0709@gmail.com>
2022-05-26 22:57:24 +0900

    최필경 feat:마이페이지 내활동에서 댓글 삭제기능 추가

--- src/components/organisms/MyCommentEdit.tsx
+++ src/components/organisms/MyCommentEdit.tsx
@@ -1,8 +1,10 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { useAppDispatch } from '../../redux/configureStore';
 import { RootState } from '../../redux/configureStore';
 import { getMyCommentDB } from '../../redux/modules/mypage';
+import { commentActionCreators } from '../../redux/modules/comment';
+import { RoundBox, Text, Button } from '../atoms';
 
 const MyCommentEdit = () => {
   const appDispatch = useAppDispatch();
@@ -13,6 +15,19 @@ const MyCommentEdit = () => {
   const { myCommentList, isMyCommentListLoaded } = useSelector(
     (store: RootState) => store.mypage
   );
+
+  const [toggle, setToggle] = useState(false);
+  const [commentsId, setCommentsId] = useState(0);
+
+  const handleToggle = (commentsId: number) => {
+    setToggle(!toggle);
+    setCommentsId(commentsId);
+  };
+
+  const handleDeleteComment = () => {
+    appDispatch(commentActionCreators.deleteCommentDB(commentsId));
+  };
+
   return (
     <div>
       {myCommentList.map((comment, index) => {
@@ -20,11 +35,42 @@ const MyCommentEdit = () => {
           <div className='m-2 flex' key={index}>
             <div className='flex justify-between w-full'>
               <p>{comment.content}</p>
-              <button>더보기</button>
+              <button
+                onClick={() => {
+                  handleToggle(comment.commentsId);
+                }}
+              >
+                더보기
+              </button>
             </div>
           </div>
         );
       })}
+      {toggle === true ? (
+        <div className='fixed z-10 touch-none top-0 left-0 w-full h-full bg-[rgba(0,0,0,0.3)]'>
+          <RoundBox round='mainModal' className='flex flex-col pb-36 mt-80'>
+            <Text className='text-subH33 font-500'>
+              어떤 작업을 하시겠어요?
+            </Text>
+            <Button
+              className='text-white font-500 text-body'
+              type='brownPType'
+              onClick={handleDeleteComment}
+            >
+              삭제하기
+            </Button>
+            <Button
+              className='mt-4 text-gray-400 shadow-lg'
+              type='bg-gray60'
+              onClick={handleToggle}
+            >
+              닫기
+            </Button>
+          </RoundBox>
+        </div>
+      ) : (
+        <></>
+      )}
     </div>
   );
 };
