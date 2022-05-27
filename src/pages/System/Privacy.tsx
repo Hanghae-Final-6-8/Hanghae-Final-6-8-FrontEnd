8ae600ec2031db2936449a3e961ec364fe718d44
Juhwan <dev.juhwanlee@gmail.com>
2022-05-27 12:57:32 +0900

    이주환 style : 개인보호 처리방침 기본 레이아웃 추가

--- src/pages/System/Privacy.tsx
+++ src/pages/System/Privacy.tsx
@@ -1,5 +1,14 @@
+import { logoCopick } from '../../assets/logo';
+
 const Privacy = () => {
-  return <>개인정보 처리 방침</>;
+  return (
+    <>
+      <header className='mt-10 mx-auto'>
+        <img className='h-20 mx-auto' src={logoCopick} />
+      </header>
+      <div className='mt-5 px-5'>개인정보 처리 방침</div>
+    </>
+  );
 };
 
 export default Privacy;
