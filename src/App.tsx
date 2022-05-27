95ce7ce80e58acdddb6c7992aa8a04f2e2935108
Juhwan <dev.juhwanlee@gmail.com>
2022-05-27 16:15:00 +0900

    이주환 feat : 전역적인 spinner 등록

--- src/App.tsx
+++ src/App.tsx
@@ -28,6 +28,8 @@ import { useAppDispatch } from './redux/configureStore';
 import { auth } from './redux/modules/user';
 import { getAccessTokenFromCookie } from './utils/cookie';
 import ScrollToTop from './utils/ScrollToTop';
+import instance from './lib/axios';
+import { isLoaded, isLoading } from './redux/modules/global';
 
 const Main = lazy(() => import('./pages/Main/Main'));
 const Login = lazy(() => import('./pages/LoginPage/Login'));
@@ -51,10 +53,34 @@ function App() {
     isToken && appDispatch(auth());
   }, [isToken, appDispatch]);
 
+  useEffect(() => {
+    instance.interceptors.request.use(
+      function (config) {
+        appDispatch(isLoading());
+        return config;
+      },
+      function (error) {
+        appDispatch(isLoaded());
+        return Promise.reject(error);
+      }
+    );
+    instance.interceptors.response.use(
+      function (config) {
+        appDispatch(isLoaded());
+        return config;
+      },
+      function (error) {
+        appDispatch(isLoaded());
+        return Promise.reject(error);
+      }
+    );
+  });
+
   return (
     <>
       <ScrollToTop />
       <Suspense fallback={<Spinner />}>
+        <Spinner />
         <Routes>
           <Route path='/main' element={<Main />} />
           <Route path='/main/privacy' element={<Privacy />} />
--- src/components/organisms/Spinner.tsx
+++ src/components/organisms/Spinner.tsx
@@ -4,14 +4,23 @@ import { RootState } from '../../redux/configureStore';
 import { GridBox } from '../atoms';
 
 const Spinner = () => {
-  const global = useSelector((state: RootState) => state.global);
+  const { loading } = useSelector((state: RootState) => state.global);
+
   return (
     <>
-      <GridBox className='h-full' type='flexBasic'>
-        <div className='m-auto'>
-          <img src={spinnerBasic} />
-        </div>
-      </GridBox>
+      {loading ? (
+        <GridBox
+          className='absolute w-full  h-full z-50 touch-none'
+          type='flexBasic'
+        >
+          <div className='bg-white absolute opacity-60 h-full w-full'>
+            {/*  */}
+          </div>
+          <div className='m-auto z-10 bg-brownS01bg rounded-full shadow-contents'>
+            <img className='w-24' src={spinnerBasic} />
+          </div>
+        </GridBox>
+      ) : null}
     </>
   );
 };
