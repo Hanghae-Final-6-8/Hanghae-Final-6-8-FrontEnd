0b8263335857ccd7183acc85c9898933f27bf17f
Juhwan <dev.juhwanlee@gmail.com>
2022-05-30 22:35:12 +0900

    이주환 feat : 팝업 유효시간 24시간으로 설정

--- src/components/atoms/MobileBanner.tsx
+++ src/components/atoms/MobileBanner.tsx
@@ -1,10 +1,33 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { mobileBanner } from '../../assets/images';
+
 const MobileBanner = () => {
-  const [isShow, setIsShow] = useState<boolean>(true);
+  const [isShow, setIsShow] = useState<boolean>(false);
+
+  useEffect(() => {
+    const closeAt = Number(localStorage.getItem('closePopup'))!;
+    const whenWillBeExpired = closeAt + 1000 * 60 * 60 * 24;
+
+    const currentTime = Math.floor(new Date().getTime());
+
+    // 처음 들어오면 팝업을 띄움
+    if (Number.isNaN(closeAt)) {
+      setIsShow(true);
+    }
+
+    // 제한 시간을 지났으므로 팝업을 띄움
+    if (whenWillBeExpired < currentTime) {
+      setIsShow(true);
+    }
+  }, []);
+
   const handleToHide = () => {
     setIsShow(false);
+    const currentTime = String(new Date().getTime());
+
+    localStorage.setItem('closePopup', currentTime);
   };
+
   return (
     <>
       {isShow ? (
